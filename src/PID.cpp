1384898f8d65502ef6f212b4f5bf8e0e6c891c98
Davinder Chandhok <davinderc@users.noreply.github.com>
2017-09-11 12:59:15 -0300

    Implement PID class methods

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,4 +1,5 @@
 #include "PID.h"
+#include <iostream>
 
 using namespace std;
 
@@ -10,12 +11,22 @@ PID::PID() {}
 
 PID::~PID() {}
 
-void PID::Init(double Kp, double Ki, double Kd) {
+void PID::Init(double Kp_in, double Ki_in, double Kd_in) {
+  this->Kp = Kp_in;
+  this->Ki = Ki_in;
+  this->Kd = Kd_in;
+  this->p_error = 0;
+  this->i_error = 0;
+  this->d_error = 0;
 }
 
 void PID::UpdateError(double cte) {
+  this->d_error = p_error - cte;
+  this->p_error = cte;
+  this->i_error += cte;
 }
 
 double PID::TotalError() {
+  return (-Kp*p_error - Ki*i_error - Kd*d_error);
 }
 
